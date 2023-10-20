60188b543a8e16f246286c982259d5655734317e
Ivana Piscitelli <ivanap.ipiscitelli@gmail.com>
2023-10-20 10:23:09 +0200

    fixed footer text ui

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="flex flex-col md:flex-row items-center md:items-start justify-evenly text-center">
         <div className="w-1/6 flex flex-col items-center justify-between my-4 md:my-0">
           <h3 className="text-lg font-bold mb-2">Recognitions</h3>
-          <ul className="space-y-1">
+          <ul className="space-y-1 w-32">
             {recognitions.map((item) => (
               <li key={item.id}>
                 <a href="#" className="text-sm">
@@ -20,7 +20,7 @@ const Footer = () => {
         </div>
         <div className="w-1/6 flex flex-col items-center my-4 md:my-0">
           <h3 className="text-lg font-bold mb-2">Pages</h3>
-          <ul className="space-y-1">
+          <ul className="space-y-1 w-32">
             {navigation.map((item) => (
               <li key={item.id}>
                 <a href="#" className="text-sm">
@@ -37,7 +37,7 @@ const Footer = () => {
         </div>
         <div className="w-1/6 flex flex-col items-center my-4 md:my-0">
           <h3 className="text-lg font-bold mb-2">Contacts</h3>
-          <ul className="flex space-x-4 cursor-pointer transition-all">
+          <ul className="flex space-x-4 w-32 cursor-pointer transition-all">
             {social.map((item) => (
               <a key={item.id} href={item.href} className={`${item.icon}-icon block`}>
                 {item.name}
