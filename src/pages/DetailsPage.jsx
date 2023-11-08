f2307ac8fe4512e9fe40ef4088d52686431f53ed
Ivana Piscitelli <ivanap.ipiscitelli@gmail.com>
2023-11-08 17:56:46 +0100

    WIP: refactor DetailsPage component layout and remove unused code

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,15 +1,12 @@
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { imgDetails } from "../utils/const";
-import useResize from "../hooks/useResize";
 
 const DetailsPage = () => {
   const [image, setImage] = useState(imgDetails[0].img);
-
   const navigate = useNavigate();
   const location = useLocation();
   const { dogId } = useParams();
-  const windowWidth = useResize();
 
   useEffect(() => {
     // Quando il componente viene smontato (l'utente naviga via), sostituisco lo stato con una route che non ha parametri.
@@ -25,50 +22,38 @@ const DetailsPage = () => {
 
   return (
     <div className="container mx-auto py-32 px-16">
-      <div className="flex flex-col lg:flex-row -mx-4">
-        {/* Contenitore per l'immagine principale */}
-        <div className="lg:flex-1 md:px-4">
-          <div className="h-[40rem] lg:h-[50rem] rounded-lg bg-gray-100 mb-4 flex items-center justify-center overflow-hidden">
-            <img src={image} className="w-full h-full object-cover" />
-          </div>
-        </div>
-        {windowWidth < 960 && (
-          <div className="flex mt-4">
-            {imgDetails.map((img, index) => (
-              <div
-                key={img.id}
-                className={`flex-1 md:px-4 ${
-                  index === 0 ? "mr-[0.2rem]" : index === imgDetails.length - 1 ? "ml-[0.2rem]" : "mx-2"
+      {/* Contenitore principale con flex che avvolge tutto */}
+      <div className="flex flex-row -mx-4">
+        {/* Contenitore per le miniature */}
+        <div className="flex flex-col mr-[1.3rem]">
+          {imgDetails.map((img, index) => (
+            <div key={img.id} className="mb-6">
+              <button
+                onClick={() => setImage(img.img)}
+                className={`focus:outline-none w-16 h-16 rounded-lg bg-gray-100 flex items-start justify-center ${
+                  image === img.img ? "ring-2 ring-indigo-300 ring-inset" : ""
                 }`}
               >
-                <button
-                  onClick={() => setImage(img.img)}
-                  className={`focus:outline-none w-full rounded-lg h-24 lg:h-32 bg-gray-100 flex items-center justify-center ${
-                    image === img.img ? "ring-2 ring-indigo-300 ring-inset" : ""
-                  }`}
-                >
-                  <img
-                    src={img.img}
-                    alt={`Thumbnail ${index}`}
-                    className="h-full w-full object-cover rounded-lg overflow-hidden"
-                  />
-                </button>
-              </div>
-            ))}
+                <img src={img.img} alt={`Thumbnail ${index}`} className="object-cover rounded-lg" />
+              </button>
+            </div>
+          ))}
+        </div>
+
+        {/* Contenitore per l'immagine principale */}
+        <div className="flex flex-col lg:flex-row">
+          <div className="flex-1">
+            <div className="h-auto w-auto sm:w-[30rem] rounded-lg bg-gray-100 mb-4 flex items-center justify-center overflow-hidden">
+              <img src={image} className="w-full h-full object-cover min-h-[21rem]" />
+            </div>
           </div>
-        )}
-
-        {/* Contenitore per il testo e le miniature */}
-
-        <div className="lg:flex-1 md:px-4 mt-8 md:mt-6">
-          <h2 className="mb-2 leading-tight tracking-tight font-bold text-gray-800 text-2xl lg:text-3xl">
-            Sunday – Ch. Cosmos Septimo Dia
-          </h2>
-          <p className="text-gray-500 text-sm">Sire: Whitesunday’s Special Edition “Rush”</p>
-
-          <div className="flex items-center space-x-4 my-4">
+          {/* Contenitore per il testo */}
+          <div className="lg:flex-1 px-4">
+            <h2 className="mb-4 leading-tight tracking-tight font-bold text-gray-800 text-2xl lg:text-3xl">
+              Sunday – Ch. Cosmos Septimo Dia
+            </h2>
             <div>
-              <div className="rounded-lg bg-gray-100 flex-col py-2 px-3 text-sm leading-relaxed">
+              <div className="rounded-lg flex-col mt-6 py-2 px-3 text-sm leading-loose">
                 <p>
                   <strong>Data di Nascita:</strong> 15 07 2012
                 </p>
@@ -93,32 +78,6 @@ const DetailsPage = () => {
               </div>
             </div>
           </div>
-          <p className="text-gray-500">
-            Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Vitae exercitationem porro saepe ea harum
-            corrupti vero id laudantium enim, libero blanditiis expedita cupiditate a est.
-          </p>
-
-          {/* Spostato qui il contenitore delle miniature */}
-          {windowWidth > 959 && (
-            <div className="flex mx-2 mt-4">
-              {imgDetails.map((img, index) => (
-                <div key={img.id} className="flex-1 md:px-2">
-                  <button
-                    onClick={() => setImage(img.img)}
-                    className={`focus:outline-none w-full rounded-lg h-24 lg:h-32 bg-gray-100 flex items-center justify-center ${
-                      image === img.img ? "ring-2 ring-indigo-300 ring-inset" : ""
-                    }`}
-                  >
-                    <img
-                      src={img.img}
-                      alt={`Thumbnail ${index}`}
-                      className="h-full w-full object-cover rounded-lg overflow-hidden"
-                    />
-                  </button>
-                </div>
-              ))}
-            </div>
-          )}
         </div>
       </div>
     </div>
